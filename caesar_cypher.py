78217957a2adb3f929b4b6d8d5db5d9ca44774a2
Sara Fontes <sfontes94@gmail.com>
2024-07-29 16:33:58 +0100

    Project stage 1

---
+++ caesar_cypher.py
@@ -0,0 +1,41 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1 # Reverse the shift for decoding
+    # The rest of the code handles both encoding and decoding
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char # Preserve non-alphabetic characters
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+
+from logo import logoImg
+print(logoImg)
+
+should_end = False
+while not should_end:
+
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+
+  # Ensure the shift is within the range of the alphabet length
+  shift = shift % 26
+
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+  if restart == "yes":
+    should_end = False
+  elif restart == "no":
+    should_end = True
+    print("Goodbye!")
+    break
+  else:
+    break
